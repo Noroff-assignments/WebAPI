68c3ddf6bf0da7b427b3fcc2ae44ac1c79d0fd75
rene-marcker <rene-marcker@hotmail.com>
2023-03-01 13:14:45 +0100

    feats: implemented a model for movie

---
+++ WebAPI/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(1), MaxLength(150)]
+        public string Title { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public int ReleaseYear { get; set; }
+        [Required, MinLength(3), MaxLength(736)]
+        public string Director { get; set; }
+        [Url]
+        public string? PosterURL { get; set; }
+        [Url]
+        public string? TrailerURL { get; set; }
+    }
+}
