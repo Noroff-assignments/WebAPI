55f8152fb8c23d3488c3cad3b46e54716bf7bcdc
Michael Piepgras Neergaard <michael@neergaard.dk>
2023-03-01 23:17:52 +0100

    feat: character profile

---
+++ WebAPI/Profiles/CharacterProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using WebAPI.Models;
+
+namespace WebAPI.Profiles
+{
+    public class CharacterProfile : Profile
+    {
+        public CharacterProfile()
+        {
+            CreateMap<CreateCharacterDto, Character>();
+            CreateMap<Character, ReadCharacterDto>()
+                .ForMember(dto => dto.Movies, options =>
+                options.MapFrom(characterDomain => characterDomain.Movies.Select(movie => $"api/v1/movies/{movie.Id}").ToList()));
+            CreateMap<UpdateCharacterDto, Character>();
+        }
+    }
+}
