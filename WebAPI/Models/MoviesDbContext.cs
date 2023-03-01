29415e9f60ce598f7ab76b68d6887a1b3d06caf7
rene-marcker <rene-marcker@hotmail.com>
2023-03-01 13:51:57 +0100

    feats: implemented the Dbcontext with fields and sql connection string

---
+++ WebAPI/Models/MoviesDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class MoviesDbContext : DbContext
+    {
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Character> Character { get; set; }
+        public DbSet<Franchise> Franchise { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = "localhost\\SQLEXPRESS",
+                InitialCatalog = "MovieAPI",
+                IntegratedSecurity = true,
+                TrustServerCertificate = true
+            };
+            return builder.ToString();
+        }
+    }
+}
