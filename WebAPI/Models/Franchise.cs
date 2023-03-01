ce60590decbc389a1f8bd11e219c89d1ede55255
rene-marcker <rene-marcker@hotmail.com>
2023-03-01 13:21:32 +0100

    feats: implemented model for franchise

---
+++ WebAPI/Models/Franchise.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Franchise
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(2), MaxLength(25)]
+        public string Name { get; set; }
+        [MaxLength(400)]
+        public string? Description { get; set; }
+
+    }
+}
