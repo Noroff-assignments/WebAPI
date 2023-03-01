3327007992d27eafc4ef1079f61e4e308b92d984
rene-marcker <rene-marcker@hotmail.com>
2023-03-01 13:23:33 +0100

    feats: implemented model for character

---
+++ WebAPI/Models/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Character
+    {
+        [Key]
+        public int Id { get; set; }
+        // found the shortest and longest name in the world
+        [Required, MinLength(3), MaxLength(736)]
+        public string FullName { get; set; }
+
+        public string? Alias { get; set; }
+        public string? Gender { get; set; }
+        [Url]
+        public string? PictureURL { get; set; }
+    }
+}
