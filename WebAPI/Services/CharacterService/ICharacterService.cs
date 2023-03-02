761a25ecf02aa71e0b6757d66fd5756bd8544b3c
Michael Piepgras Neergaard <michael@neergaard.dk>
2023-03-02 11:26:36 +0100

    feat: services & service interfaces
    methods not implemented yet

---
+++ WebAPI/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        Task<IEnumerable<Character>> GetAllCharacters();
+        Task<Character> GetCharacterById(int id);
+        Task<Character> AddCharacter(Character character);
+        Task DeleteCharacter(int id);
+        Task<Character> UpdateCharacter(Character character);
+    }
+}
